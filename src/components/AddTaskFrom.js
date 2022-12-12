5fbd638b2a8b8f23032bd5ae98869520bc7a8459
JasleenKaur2000 <jasleenkaur46353@gmail.com>
2022-12-12 18:15:49 +0530

    completed

---
+++ src/components/AddTaskFrom.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AddTaskForm({ newTask, setNewTask, addTask }) {
+  return (
+    <>
+      {/* Add Task */}
+      <div className="row">
+        <div className="col">
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className="form-control form-control-lg"
+          />
+          <button onClick={addTask} className="btn btn-lg btn-success mr-10">
+            Add Task
+          </button>
+        </div>
+      </div>
+      <br />
+    </>
+  );
+}
+
+export default AddTaskForm;
